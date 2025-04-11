32060ea6a368e0086097a2c479565583a228b38e
Blockchainnewbie <haberland-m-d@outlook.com>
2025-04-11 10:59:17 +0200

    Ergänze die HelloMe-Klasse um zusätzliche Ausgaben zur Lernmotivation.

--- sturdy-guacamole/HelloMe.py
+++ sturdy-guacamole/HelloMe.py
@@ -3,6 +3,9 @@ class HelloMe:
 
 def me():
     print("Hello, Me!")
+    print("This is a simple Python script.")
+    print("I am learning to buld my own packages.")
+
 
 if __name__ == '__main__':
     me()
